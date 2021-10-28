15d3527cbcf90ebac25f79f0117823d5c8e5ee39
Alice <80988747+Licettte@users.noreply.github.com>
2021-10-28 07:09:06 +0300

    1

---
+++ firstapplication1/src/main/java/com/example/firstapplication1/Firstapplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.firstapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstapplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstapplication1Application.class, args);
+	}
+
+}
