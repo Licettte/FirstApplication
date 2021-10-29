17c6429bd53bad22a2936985725137dc99d72e26
Alice <80988747+Licettte@users.noreply.github.com>
2021-10-29 12:57:24 +0300

    1

---
+++ firstapplication1/src/main/java/com/example/firstapplication1/Controller.java
@@ -0,0 +1,26 @@
+package com.example.firstapplication1;
+
+import com.example.firstapplication1.model.BankAccount;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @Value("${url}")
+  String URL;
+
+  @GetMapping("/getAccount")
+  public BankAccount bankAccount() {
+    BankAccount bankAccount = new BankAccount();
+    bankAccount.setAccountNumber(123);
+    bankAccount.setFirstName("Маша");
+    bankAccount.setLastName("Кляйн");
+    bankAccount.setPatronymic("Сергеевна");
+    bankAccount.setUuid(UUID.randomUUID());
+    System.out.println(URL);
+    return bankAccount;
+  }
+}
--- firstapplication1/src/main/java/com/example/firstapplication1/Firstapplication1Application.java
+++ firstapplication1/src/main/java/com/example/firstapplication1/Firstapplication1Application.java
@@ -8,6 +8,8 @@ public class Firstapplication1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Firstapplication1Application.class, args);
+
+
 	}
 
 }
